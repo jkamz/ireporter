f201febaef0a908ccbbd73dd119ec808a4373f04
Elisha Misoi <e.kmisoi@outlook.com>
2019-03-07 10:05:23 +0300

    [Feature #164272428] create redflag record

--- api/config/settings.py
+++ api/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import django_heroku
 import logging
 import environ
 from django.utils.translation import gettext_lazy as _
@@ -54,12 +55,13 @@ USE_X_FORWARDED_HOST = env.bool('DJANGO_USE_X_FORWARDED_HOST', default=True)
 # https://docs.djangoproject.com/en/2.0/ref/settings/#installed-apps
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.sites',
     'rest_framework',
     'rest_framework.authtoken',
@@ -72,7 +74,6 @@ INSTALLED_APPS = [
     'django_extensions',
     'django.contrib.postgres',
     'phonenumber_field',
-    'rest_framework_swagger',
     'incidents',
 ]
 
@@ -129,6 +130,7 @@ DJOSER = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -316,9 +318,9 @@ MOBILE_ACTIVATION_TOKEN_LENGTH = 6
 MOBILE_ACTIVATION_TOKEN_HASH_ALGORITHM = 'sha256'
 
 # Test Settings
-TEST_PAYLOAD_PATH = str(API_DIR) + '/utils/test/'
-TEST_DATA_PATH = TEST_PAYLOAD_PATH + 'data/'
-TEST_RUNNER = 'utils.test.test_runner.CMTestRunner'
+#TEST_PAYLOAD_PATH = str(API_DIR) + '/utils/test/'
+#TEST_DATA_PATH = TEST_PAYLOAD_PATH + 'data/'
+#TEST_RUNNER = 'utils.test.test_runner.CMTestRunner'
 
 # Site Reliability Team
 # https://docs.djangoproject.com/en/2.0/ref/settings/#admins
@@ -420,17 +422,4 @@ if DJANGO_ENV == 'production':
         'DSN':
         SENTRY_DSN
     }
-
-# Swagger Settings
-SWAGGER_SETTINGS = {
-   'SHOW_REQUEST_HEADERS': True,
-   'USE_SESSION_AUTH': False,
-   'DOC_EXPANSION': 'list',
-   'SECURITY_DEFINITIONS': {
-       'api_key': {
-           'type': 'apiKey',
-           'in': 'header',
-           'name': 'Authorization'
-       }
-   }
-}
+django_heroku.settings(locals())
