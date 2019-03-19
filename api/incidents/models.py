bcb7cdb8780f190edaa50eaf3a3902051ce72bfd
jkamz <jkamz6432@gmail.com>
2019-03-19 04:04:39 +0300

    [Finishes #164327212] update redflag record

--- api/incidents/models.py
+++ api/incidents/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid
 from django.contrib.auth import get_user_model
 
+
 class RedflagModel(models.Model):
     """The incident model."""
 
@@ -9,15 +10,14 @@ class RedflagModel(models.Model):
     incident_type = 'red-flag'
     title = models.CharField(max_length=128)
     location = models.CharField(max_length=150)
-    status = models.CharField(max_length=30)
+    status = models.CharField(max_length=30, default="draft")
     Image = models.URLField(blank=True, null=True)
     Video = models.URLField(blank=True, null=True)
     comment = models.TextField(blank=False, null=False)
     createdOn = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
-        return "{}".format(self.title)        
+        return "{}".format(self.title)
 
     class Meta:
         ordering = ["-createdOn"]
-
